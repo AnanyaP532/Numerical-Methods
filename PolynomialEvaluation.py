b6df4f3d2fed6a199a2ff67c58bed9b047c79856
Ananya Priya <135134362+chloegmx@users.noreply.github.com>
2023-06-02 14:35:17 +0530

    Create PolynomialEvaluation.py
    Polynomial Evaluation using Synthetic division

---
+++ PolynomialEvaluation.py
@@ -0,0 +1,20 @@
+pol= [6,12,2,25]                         #for polynomial 6x^3 + 12x^2 + 2x + 25
+x= len(pol)-1                            
+n= len(pol)
+result=0
+for i in range (0,n):
+    result= pol[i]+ result*x
+print(result)
+
+
+
+#Reading files
+from numpy import loadtxt
+pol = loadtxt("polynomials.txt")
+x= len(pol)-1
+n= len(pol)
+result=0
+for i in range (0,n):
+    result= pol[i]+ result*x
+print(result)
+print(pol)
